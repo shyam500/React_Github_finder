f89b6c9ff6642c675b28e955ccbb2d9ba0b9f006
shyam500 <shyamppk2000@gmail.com>
2022-03-20 19:50:23 +0530

    added favicon

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import classes from "./Home.module.css";
+
+const Home = () => {
+  return (
+    <section>
+      <h1>Github Finder</h1>
+    </section>
+  );
+};
+export default Home;
---
+++ src/components/UserList.jsx
@@ -0,0 +1,10 @@
+import classes from "./UserList.module.css";
+
+const UserList = () => {
+    return(
+        <section className={classes.User_container}>
+            <h2>Top users</h2>
+        </section>
+    )
+};
+export default UserList;
